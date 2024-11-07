f3f9cc03c1c58868e87190264ac4d7a78d7ea3d9
Allister Beharry <allister.beharry@gmail.com>
2024-11-06 23:09:31 -0400

    Add CLI entry point and options.

---
+++ gen.cmd
@@ -0,0 +1,18 @@
+@echo off
+pushd
+@setlocal
+set ERROR_CODE=0
+
+REM From Alec Mev https://superuser.com/questions/35698/how-to-supress-terminate-batch-job-y-n-confirmation/715798#715798
+IF [%JUSTTERMINATE%] == [OKAY] (
+    SET JUSTTERMINATE=
+    src\SharpGambit.Generator\bin\Release\net8.0\SharpGambit.Generator.exe %*
+) ELSE (
+    SET JUSTTERMINATE=OKAY
+    CALL %0 %* <NUL
+)
+
+:end
+@endlocal
+popd
+exit /B %ERROR_CODE%
